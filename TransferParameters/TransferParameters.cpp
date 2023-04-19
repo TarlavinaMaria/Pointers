70ec39adf9fc5268134a05ad56a7ffe2f67c977c
Мария Тарлавина <tarlavina50@mail.ru>
2023-04-19 19:39:51 +0700

    Encoding

--- TransferParameters/TransferParameters.cpp
+++ TransferParameters/TransferParameters.cpp
@@ -1,7 +1,7 @@
 ﻿#include<iostream>
 using namespace std;
 
-#define delimiter "\n--------------------------------------------\n";
+#define delimiter "\n-------------------------------------------\n"
 
 void Exchange(int a, int b);
 
@@ -10,14 +10,19 @@ void main()
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
+	cout << &a << "\t" << &b << endl;
 	cout << delimiter << endl;
 	Exchange(a, b);
+	cout << delimiter << endl;
 	cout << a << "\t" << b << endl;
+	cout << &a << "\t" << &b << endl;
 }
 
 void Exchange(int a, int b)
 {
+	cout << &a << "\t" << &b << endl;
 	int buffer = a;
 	a = b;
 	b = buffer;
+	cout << a << "\t" << b << endl;
 }
