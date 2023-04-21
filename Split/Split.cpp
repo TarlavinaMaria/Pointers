5d6e39abb2c50bea95dc6bb7b04479fda93756fc
Мария Тарлавина <tarlavina50@mail.ru>
2023-04-21 12:59:45 +0700

    Encoding

--- Split/Split.cpp
+++ Split/Split.cpp
@@ -1,17 +1,25 @@
 #include<iostream>
+#include <string>
+#include <iomanip>
+
 using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+void Split(int arr[], const int n);
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n = 10;
+	int n;
 	cout << "¬ведите размер массива: "; cin >> n;
-	int arr[n];
+	int* arr = new int[n];
+	
 	FillRand(arr, n);
 	Print(arr, n);
+	Split(arr, n);
+	
 	delete[] arr;
 }
 
@@ -30,4 +38,28 @@ void Print(int arr[], const int n)
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}
+void Split(int arr[], const int n)
+{
+	int* odd = new int[n];
+	int* even = new int[n];
+	int oddcnt = 0;
+	int evencnt = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2)
+		{
+			odd[oddcnt++] = arr[i];
+		}
+		else
+		{
+			even[evencnt++] = arr[i];
+		}
+	}
+	cout << "odd:" << *odd << endl;
+	cout << "even:" << *even << endl;
+
+	delete[] odd;
+	delete[] even;
 }
